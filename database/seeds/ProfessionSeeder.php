cb034382fe18d35c9a28f5edd0f03b88c1df1223
Berly Pumaccajia <b.pumacajia.cz@gmail.com>
2018-12-12 14:42:13 -0500

    Constructor de consultas SQL de Laravel

--- database/seeds/ProfessionSeeder.php
+++ database/seeds/ProfessionSeeder.php
@@ -12,10 +12,9 @@ class ProfessionSeeder extends Seeder
      */
     public function run()
     {
-    	DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-    	DB::table('professions')->truncate();
-
-    	// DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    	// DB::insert('INSERT INTO professions (title) VALUES (:title)', [ 
+    	// 	'title' => 'Desarrollador back-end'
+    	// ]);
 
         DB::table('professions')->insert([
         	'title' => 'Desarrollador Back-end',
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,10 +12,29 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+
+    	// $professions = DB::select('SELECT id FROM professions WHERE title = :title LIMIT 0,1', [
+    	// 	'title' => 'Desarrollador back-end',
+    	// ]);
+
+    	$professionId = DB::table('professions')
+    					->whereTitle('Desarrollador back-end')
+    					->value('id');
+
+    	$professionId2 = DB::table('professions')->where('title','Desarrollador front-end')->value('id');
+
+
         DB::table('users')->insert([
         	'name' => 'Berly Pumaccajia',
         	'email' => 'berly@pumacajia.com',
         	'password' => bcrypt('laravel'),
+        	'profession_id' => $professionId,
         ]);
+
+
+
+        DB::insert('INSERT INTO users(name, email, password, profession_id) VALUES (:name, :email,:password, :profession_id)', ['name' => 'serafin', 'email' => 'era@fin.com', 'password' => bcrypt('laravel'), 'profession_id' => $professionId2]);
+
+        DB::delete('DELETE FROM users WHERE name = "serafin"');
     }
 }
