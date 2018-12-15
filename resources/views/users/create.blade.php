831b9d1da5f330590bb426fb6932637901a1627f
Berly Pumaccajia <b.pumacajia.cz@gmail.com>
2018-12-15 11:33:59 -0500

    Formulario para agregar usuarios

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,11 +6,35 @@
 			<h1>Crear usuario</h1>
 
 			<form action="{{ url('/usuarios')}}" method="POST">
-				{!!csrf_field()!!}
-				<button type="submit">Crear usuario</button>
+			{!!csrf_field()!!}
+
+			  <div class="form-row">
+			    <div class="form-group col-md-6">
+			      <label for="name">Nombre:</label>
+			      <input type="text" class="form-control" id="name" name="name" placeholder="Tu nombre aquí">
+			    </div>
+			  </div>
+
+			  <div class="form-row">
+			    <div class="form-group col-md-6">
+			      <label for="email">Email:</label>
+			      <input type="email" class="form-control" id="email" name="email" placeholder="user@compañia.com">
+			    </div>
+			  </div>
+
+			  <div class="form-row">
+			  	<div class="form-group col-md-6">
+			      <label for="password">Password:</label>
+			      <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+			    </div>
+			  </div>
+		  
+
+
+				<button type="submit" class="btn btn-primary">Crear usuario</button>
 				
 			</form>
-			<p>
+			<p class="mt-3">
 				<a href="{{route("users.index")}}">Regresar</a>
 			</p>
 
