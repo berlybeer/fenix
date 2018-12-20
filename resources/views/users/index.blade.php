c45672d59499562d39088fe77fa9f66be072e983
Berly Pumaccajia <b.pumacajia.cz@gmail.com>
2018-12-19 22:24:59 -0500

    Agregar estilos Bootstrap 4 al listado de usuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,29 +1,52 @@
 @extends('layout')
 @section('title', 'Usuarios')
 @section('content')
-	<h1>{{ $title }}</h1>
-	<p>
-		<a href="{{route('users.create')}}">Nuevo usuario</a>
-	</p>
-	<hr>
 
-	<ul>
-		@forelse($users as $user)
-		<li>
-			{{ $user->name}}, ({{ $user->email}})
-			<a href="{{ route('users.show', $user)}}">Ver detalles</a>|
-			<a href="{{ route('users.edit', $user)}}">Editar</a>|
+
+	<div class="d-flex justify-content-between align-items-end mb-3">
+	<h1 class="pb-1">{{ $title }}</h1>
+	<p>
+		<a href="{{route('users.create')}}" class="btn btn-primary">Nuevo usuario</a>
+	</p>
+	</div>
+
+	@if($users->isNotEmpty())
+
+	<table class="table">
+	  <thead class="thead-dark">
+	    <tr>
+	      <th scope="col">#</th>
+	      <th scope="col">Nombre</th>
+	      <th scope="col">Correo</th>
+	      <th scope="col">Acciones</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	  @foreach($users as $user)
+	    <tr>
+	      <th scope="row">{{$user->id}}</th>
+	      <td>{{$user->name}}</td>
+	      <td>{{$user->email}}</td>
+	      <td>
+
 			<form action="{{route('users.destroy' , $user)}}" method="POST">
 				{{csrf_field()}}
 				{{method_field('DELETE')}}
-				<button type="submit">Eliminar</button>
+				<a href="{{ route('users.show', $user)}}" class="btn btn-link"><i class="far fa-eye"></i></a>
+				<a href="{{ route('users.edit', $user)}}"class="btn btn-link"><i class="fas fa-pencil-alt"></i></a>
+				<button type="submit" class="btn btn-link"><i class="fas fa-trash-alt"></i></button>
 			</form>
-		
-		</li>
-		@empty
-		<p>No hay usuarios registrados</p>
-		@endforelse
-	</ul>
+	      </td>
+	    </tr>
+	  @endforeach
+	  </tbody>
+	</table>
+	@else
+		<p>No hay usuario registrados</p>
+	@endif
+
+	<hr>
+
 @endsection
 
 
