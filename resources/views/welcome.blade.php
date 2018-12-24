108014e45c02bd6e691ccc7a278dca95a8757d0a
Berly Pumaccajia <b.pumacajia.cz@gmail.com>
2018-12-24 15:30:16 -0500

    Creación de pruebas end to end con laravel dusk

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Feniz de redimiendo las zenizas
+                   Laravel
                 </div>
 
                 <div class="links">
